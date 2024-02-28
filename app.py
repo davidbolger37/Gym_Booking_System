d66c23228e81967ff7f8e6058b51475925e390dc
epalnat <natalia.palej@ericsson.com>
2024-02-28 17:55:55 +0000

    Login and Register page styled

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 
 USERNAME = 'root'
-PASSWORD = ''
+PASSWORD = 'root'
 HOST = 'localhost'
-DB_NAME ='database_name' #replace
+DB_NAME = 'database_name'  # replace
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+USERNAME+':'+PASSWORD+'@'+HOST+'/'+DB_NAME
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DB_NAME
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Item(db.Model):
-    __tablename__ = 'name_of_table' #replace
-    id = db.Column(db.Integer, primary_key=True) #replace
-    name = db.Column(db.String(100)) #replace
+class UserRole(Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+
+class User(db.Model):
+    __tablename__ = 'user_table'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    user_email = db.Column(db.String(120), unique=True, nullable=False)
+    user_password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.Enum(UserRole), nullable=False)
+
+    def __init__(self, first_name, last_name, user_email, user_password, role):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_email = user_email
+        self.user_password(user_password)
+        self.role = role
+
+    @classmethod
+    def count_users(cls):
+        return cls.query.count()
 
 
-@app.route('/')
-def hello():
-    return 'Hello!'
+# @app.route('/')
+# def hello():
+#     return 'Hello!'
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
