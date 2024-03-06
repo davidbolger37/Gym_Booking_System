14f7095f02f553973d49d810485ff4541f213b1b
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-03-06 10:47:11 +0000

    Update index.js
    Attach event listeners to sidebar links.

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -3,15 +3,12 @@ function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Define the closeNav function
-    function closeNav() {
-        document.getElementById('mySidebar').style.width = '0';
-    }
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
 
-    // Attach event listeners to sidebar links
+document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.sidebar a').forEach(item => {
         item.addEventListener('click', closeNav);
     });
 });
-
