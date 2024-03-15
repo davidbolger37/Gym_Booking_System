61a2b684da07230c7cc4d43202c5b11bc2662fd0
david <david.bolger@ericsson.com>
2024-03-15 14:01:42 +0000

    fixed inconsistant attributes and info names, updated script to run properly with new changes

--- website/static/sql/Database CRUD commands.sql
+++ website/static/sql/Database CRUD commands.sql
@@ -1,47 +1,41 @@
 -----------------------------COMMANDS-----------------------------
 
 -------------------------USEFULL_COMMANDS-------------------------
-SHOW DATABSES; = list all databases available
+SHOW DATABASES; = list all databases available
 SHOW TABLES; = list all tables
-ctrl c = exit
-DROP DATABASE test_db; = deletes a databse and its content
+ctrl c = exit --or type exit
+DROP DATABASE test_db; = deletes a database and its content
 DROP TABLE test_table; = deletes a table from a database
 
 -----------------------------CREATE-----------------------------
 
-INSERT INTO users (first_name, last_name, email, password, role)
+INSERT INTO users (first_name, last_name, user_email, user_password, role)
 VALUES ('John', 'Doe', 'john@example.com', 'password123', 'user');
 
-INSERT INTO users (first_name, last_name, email, password, role)
+INSERT INTO users (first_name, last_name, user_email, user_password, role)
 VALUES ('Philip', 'Darcy', 'philip@example.com', 'password456', 'admin');
 
-INSERT INTO class (class, day, time_duration)
+INSERT INTO class (class_name, day, time_duration)
 VALUES ('Yoga', 'Monday', '10:00 AM - 11:00 AM');
 
-INSERT INTO class (class, day, time_duration)
-VALUES ('Spin', 'Friday', '12:00 AM - 1:00 PM');
+INSERT INTO class (class_name, day, time_duration)
+VALUES ('Spin', 'Friday', '12:00 AM - 15:00 PM');
 
-INSERT INTO booking (user_id, class_id, booking_at)
-VALUES (1, 1, NOW());
+INSERT INTO booking (user_id, class_id, user_email, class_name, day, time_duration, booking_at)
+(user_id, class_id, 'alice@example.com', 'Pilates', 'Friday', '9:00 AM - 10:00 AM', NOW());
 
-INSERT INTO booking (user_id, class_id, booking_at)
-VALUES (
-    (SELECT id FROM users WHERE first_name = 'Jane' LIMIT 1),
-    (SELECT id FROM class WHERE class = 'Yoga' LIMIT 1),
-    NOW()
-);
 
 -----------------------------READ-----------------------------
 
 SELECT * FROM users;
 SELECT first_name FROM users;
 SELECT last_name FROM users;
-SELECT password FROM users;
-SELECT email FROM users;
+SELECT user_password FROM users;
+SELECT user_email FROM users;
 SELECT role FROM users;
 
 SELECT * FROM class;
-SELECT class FROM class;
+SELECT class_name FROM class;
 SELECT day FROM class;
 SELECT time_duration FROM class;
 
@@ -49,42 +43,53 @@ SELECT * FROM booking;
 SELECT user_id FROM booking;
 SELECT class_id FROM booking;
 SELECT booking_at FROM booking;
+SELECT user_email FROM booking;
+SELECT class_name FROM booking;
+SELECT day FROM booking;
+SELECT time_duration FROM booking;
 
 
 -----------------------------UPDATE-----------------------------
 
 UPDATE users
 SET first_name = 'Johnny',
-    email = 'johnny@example.com'
+    user_email = 'johnny@example.com'
 WHERE first_name = 'John';
 
 UPDATE users
 SET first_name = 'John',
-    email = 'john@example.com'
+    user_email = 'john@example.com'
 WHERE id = 2;
 
 UPDATE class
-SET class = 'Pilates',
+SET class_name = 'Pilates',
     time_duration = '11:00 AM - 12:00 PM'
 WHERE id = 1;
 
 UPDATE class
-SET class = 'Pilates',
+SET class_name = 'Pilates',
     time_duration = '11:00 AM - 12:00 PM'
 WHERE class = 'Yoga';
+
 
 -----------------------------DELETE-----------------------------
 
 DELETE FROM users WHERE id = 1;
 DELETE FROM users WHERE first_name = 'John';
 DELETE FROM users WHERE last_name = 'Doe';
-DELETE FROM users WHERE email = 'john@example.com';
-DELETE FROM users WHERE password = 'password123';
+DELETE FROM users WHERE user_email = 'john@example.com';
+DELETE FROM users WHERE user_password = 'password123';
 DELETE FROM users WHERE role = 'user';
 
 DELETE FROM class WHERE id = 1;
-DELETE FROM class WHERE class = 'Yoga';
+DELETE FROM class WHERE class_name = 'Yoga';
 DELETE FROM class WHERE day = 'Monday';
 DELETE FROM class WHERE time_duration = '10:00 AM - 11:00 AM';
 
 DELETE FROM booking WHERE id = 1;
+DELETE FROM booking WHERE user_id = 1;
+DELETE FROM booking WHERE class_id = 1;
+DELETE FROM booking WHERE user_email = 'john@example.com';
+DELETE FROM booking WHERE class_name = 'Yoga';
+DELETE FROM booking WHERE day = 'Monday';
+DELETE FROM booking WHERE time_duration = '10:00 AM - 11:00 AM';
--- website/static/sql/Sql_Script.sql
+++ website/static/sql/Sql_Script.sql
@@ -21,38 +21,42 @@ CREATE TABLE IF NOT EXISTS booking (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     class_id INT NOT NULL,
-    booking_at TIMESTAMP,
     user_email VARCHAR(255) NOT NULL,
     class_name VARCHAR(40) NOT NULL,
     day VARCHAR(15) NOT NULL,
     time_duration VARCHAR(40) NOT NULL,
+    booking_at TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE
 );
 
 -- Populate the 'users' table
 INSERT INTO users (first_name, last_name, user_email, user_password, role) VALUES
-('John', 'Doe', 'john@example.com', 'password123', 'user'),
 ('Jane', 'Smith', 'jane@example.com', 'abc123', 'admin'),
+('John', 'Doe', 'john@example.com', 'password123', 'user'),
+('Mason', 'Cullen', 'mason@example.com', 'mason_cullen', 'user'),
+('Devin', 'Weston', 'devin@example.com', 'westonPOP', 'user'),
 ('Alice', 'Johnson', 'alice@example.com', 'qwerty', 'user');
 
 -- Populate the 'class' table
 INSERT INTO class (class_name, day, time_duration) VALUES
 ('Yoga', 'Monday', '10:00 AM - 11:00 AM'),
-('Zumba', 'Tuesday', '6:00 PM - 7:00 PM'),
+('Zumba', 'Monday', '12:00 AM - 15:00 PM'),
+('Zumba', 'Tuesday', '18:00 PM - 19:00 PM'),
 ('Pilates', 'Wednesday', '9:00 AM - 10:00 AM'),
+('Spinning', 'Wednesday', '13:00 PM - 14:00 PM'),
 ('Yoga', 'Thursday', '10:00 AM - 11:00 AM'),
-('Zumba', 'Friday', '6:00 PM - 7:00 PM'),
-('Pilates', 'Saturday', '9:00 AM - 10:00 AM'),
-('Yoga', 'Thursday', '10:00 AM - 11:00 AM'),
-('Zumba', 'Friday', '6:00 PM - 7:00 PM'),
-('Pilates', 'Saturday', '9:00 AM - 10:00 AM'),
-('Zumba', 'Monday', '12:00 AM - 15:00 AM');
+('Pilates', 'Thursday', '10:00 AM - 11:00 AM'),
+('Spinning', 'Friday', '18:00 PM - 19:00 PM'),
+('Yoga', 'Friday', '20:00 PM - 22:00 PM'),
+('Spinning', 'Saturday', '6:00 AM - 8:00 AM'),
+('Pilates', 'Saturday', '9:00 AM - 10:00 AM');
 
 -- Populate the 'booking' table
 INSERT INTO booking (user_id, class_id, user_email, class_name, day, time_duration, booking_at) VALUES
-(1, 1, 'john@example.com', 'Yoga', 'Monday', '10:00 AM - 11:00 AM', NOW()),
-(2, 2, 'jane@example.com', 'Zumba', 'Wednesday', '6:00 PM - 7:00 PM', NOW()),
-(3, 3, 'alice@example.com', 'Pilates', 'Friday', '9:00 AM - 10:00 AM', NOW());
-
-
+(2, 1, 'john@example.com', 'Yoga', 'Monday', '10:00 AM - 11:00 AM', NOW()),
+(5, 3, 'alice@example.com', 'Zumba', 'Tuesday', '18:00 PM - 19:00 PM', NOW()),
+(1, 4, 'jane@example.com', 'Pilates', 'Wednesday', '9:00 AM - 10:00 AM', NOW()),
+(5, 8, 'alice@example.com', 'Spinning', 'Friday', '18:00 PM - 19:00 PM', NOW()),
+(4, 9, 'devin@example.com', 'Yoga', 'Friday', '20:00 PM - 22:00 PM', NOW()),
+(3, 11, 'mason@example.com', 'Pilates', 'Saturday', '9:00 AM - 10:00 AM', NOW());
